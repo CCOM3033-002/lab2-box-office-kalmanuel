ec18209a095aca92b456931bdac734423d5f7791
kalmanuel <kaleb.martinez2@upr.edu>
2024-09-06 18:53:05 +0000

    commmit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+//Kaleb M. Martinez Ortega - 801230408
+//CCOM 3033 - 002
 
+#include <iostream> 
+#include <string> 
+#include <iomanip> 
+using namespace std; 
+
+int main() 
+{
+    string movie; 
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater." << endl; 
+    
+    cout << "Movie name: "; 
+
+
+
+
+
+
+
+    return 0; 
+}
